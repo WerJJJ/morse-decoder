9c707f1abccfd2a82ad78bfebd7a4520249ea55f
Alexey Sadouski <marikari0000@gmail.com>
2020-10-18 18:15:19 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res  = expr.split('**********');
+    let finalResult = "";
+    for (let i in res){
+        let countWord = res[i].length / 10;
+        let word = [];
+        for (let j = 0; j < countWord; j++){
+            word[j] = res[i].slice(j * 10, (j+1)*10);
+            word[j] = word[j].split('');
+            let k = 0;
+            while (word[j][k] === '0'){
+                word[j].splice(0,1);
+            }
+            word[j] = word[j].join('');
+        }
+        console.log(word); 
+        let letters;
+        let result;
+        for (let j = 0; j < countWord; j++){
+            letters = [];
+            for (let z = 0; z < word[j].length; z = z + 2){
+                letters[z/2] = word[j].slice(z, z+2);
+            }
+             result = letters.map(function(item){
+                if (item == '10') return '.';
+                else if (item == '11') return '-';
+            })
+            result = result.join('');
+            finalResult += MORSE_TABLE[result];
+        }
+        finalResult = finalResult + " ";
+    }
+    return finalResult.trim();
 }
 
 module.exports = {
     decode
-}
+} 
